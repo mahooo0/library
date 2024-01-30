3bfb8112341e9dbc3a47ea8dd45a5c4994482a67
mahooo0 <143085378+mahooo0@users.noreply.github.com>
2024-01-31 03:44:48 +0400

    debuging

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,20 +1,19 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-
-  
-    // If we need pagination
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    // Swiper ayarları buraya gelecek
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Diğer ayarlar...
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
+      clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-  
-    // And if we need scrollbar
-    // scrollbar: {
-    //   el: '.swiper-scrollbar',
-    // },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
   });
+});
