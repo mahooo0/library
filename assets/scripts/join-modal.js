9e99cc2aa6c35c78001a152f7c83d82bb4c9d0ed
mahooo0 <143085378+mahooo0@users.noreply.github.com>
2024-02-05 16:55:57 +0400

    add join us fire base

--- assets/scripts/join-modal.js
+++ assets/scripts/join-modal.js
@@ -1,3 +1,4 @@
+
 let joinBtn = document.querySelector("#joinBtn");
 let joinModal = document.querySelector("#joinModal");
 document.addEventListener("DOMContentLoaded", function () {
@@ -20,3 +21,45 @@ window.addEventListener("click", function (event) {
     joinModal.style.display = "none";
   }
 });
+
+const Join_email_inp=document.querySelector("#Join_email")
+const Join_password_inp=document.querySelector("#Join_password")
+const Join_btn=document.querySelector("#Join_btn")
+
+//  join us fire bas
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+    import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyCV_voaw0_yXKkpKGQF3oLjmaLCxiLhWMA",
+      authDomain: "library-auth-d42eb.firebaseapp.com",
+      projectId: "library-auth-d42eb",
+      storageBucket: "library-auth-d42eb.appspot.com",
+      messagingSenderId: "145737395628",
+      appId: "1:145737395628:web:89b70be0447b0a50a35e09"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    Join_btn.addEventListener("click",()=>{
+      let Join_email=Join_email_inp.value
+      let Join_password=Join_password_inp.value
+      createUserWithEmailAndPassword(auth, Join_email, Join_password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log("created");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+       console.log(errorCode + errorMessage);
+      });
+
+    })
+//  join us fire bas
