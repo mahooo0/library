abaad5f91e67e64e47ee55039bbf03d7abf8f4f9
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2024-02-06 10:31:48 +0400

    change adminjs

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,29 +1,31 @@
 
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 
 const firebaseConfig = {
-apiKey: "AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c",
-authDomain: "library-35b3c.firebaseapp.com",
-projectId: "library-35b3c",
-storageBucket: "library-35b3c.appspot.com",
-messagingSenderId: "498632706422",
-appId: "1:498632706422:web:9d181dd4820520b7c01257",
-databaseURL: "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app"
+    apiKey: "AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c",
+    authDomain: "library-35b3c.firebaseapp.com",
+    projectId: "library-35b3c",
+    storageBucket: "library-35b3c.appspot.com",
+    messagingSenderId: "498632706422",
+    appId: "1:498632706422:web:9d181dd4820520b7c01257",
+    databaseURL: "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
+// HTML elementleri
 const search_Input = document.querySelector('#search_Input');
 const search_Btn = document.querySelector('#search_Btn');
 const search_variant = document.querySelector('#search_variant');
-const about_Store = document.querySelector('#about_Store');
+const book_form_add = document.querySelector('#book_form_add');
+
 const book_form_div = document.querySelector('#book_form_div'); 
 
+// Arama girdisine olay dinleyici ekleyin
 search_Input.addEventListener('input', async () => {
     const searchTerm = search_Input.value.trim();
     if (searchTerm.length == 0) {
@@ -38,6 +40,7 @@ search_Input.addEventListener('input', async () => {
     }
 });
 
+// Google Books API'sinden kitapları alan funksiya
 async function getBooks(searchTerm) {
     try {
         const response = await fetch(
@@ -53,15 +56,14 @@ async function getBooks(searchTerm) {
     }
 }
 
+// Kitap variyantlarini gosteren funksiya
 async function showBookVariants(books) {
     search_variant.innerHTML = '';
     books.forEach((book) => {
         const variantDiv = document.createElement('div');
         variantDiv.classList.add('variant');
         variantDiv.innerHTML = `
-            <img class="variant-image" src="${
-                book.volumeInfo.imageLinks?.thumbnail || '/assets/img/default.png'
-            }" alt="Book Image">
+         
             <div class="variant-details">
                 <h3>${book.volumeInfo.title}</h3>
                 <p>${
@@ -74,7 +76,7 @@ async function showBookVariants(books) {
             </div>
         `;
         variantDiv.addEventListener('click', () => {
-            search_variant.innerHTML = ''; // Hazırki məlumatları təmizləyir
+            search_variant.innerHTML = ''; 
             variantDiv.classList.remove('variant');
             variantDiv.classList.add('selected-variant');
             variantDiv.style.width = '100%';
@@ -83,9 +85,7 @@ async function showBookVariants(books) {
             variantDiv.style.marginLeft = '30px';
             variantDiv.style.gap = '40px';
 
-            const aboutStoreDiv = document.createElement('div');
-            aboutStoreDiv.classList.add('book_form');
-            aboutStoreDiv.id = 'about_Store';
+         
 
             const titleInput = book.volumeInfo.title || 'Unknown Title';
             const imageUrlInput = book.volumeInfo.imageLinks?.thumbnail || '/assets/img/default.png';
@@ -93,8 +93,8 @@ async function showBookVariants(books) {
             const authorInput = book.volumeInfo.authors || 'Unknown Author';
             const bookTypeInput = book.volumeInfo.categories || 'Unknown Type'; 
 
-            aboutStoreDiv.innerHTML = `
-                <div class="book_form" id="book_form_div"> <!-- book_form_div ekledim -->
+            book_form_div.innerHTML = `
+              
                     <div class="book_form_secion">
                         <h2 class="book_form_secion_title">Book Name</h2>
                         <input type="text" class="book_form_secion_inp" value="${titleInput}" placeholder="Angels">
@@ -109,20 +109,20 @@ async function showBookVariants(books) {
                     </div>
                     <div class="book_form_secion">
                         <h2 class="book_form_secion_title"> Description</h2>
-                        <textarea name="" id="" cols="30" rows="10" class="description_inp" placeholder="We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...">${descriptionInput}</textarea>
+                        <textarea cols="30" rows="10" class="description_inp" placeholder="We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...">${descriptionInput}</textarea>
                     </div>
                     <div class="book_form_secion">
                         <h2 class="book_form_secion_title">Book Type</h2>
                         <input type="text" class="book_form_secion_inp" value="${bookTypeInput}" placeholder="fantastic">
                     </div>
                     <button class="book_form_secion_btn">add</button>
-                </div>
+              
             `;
 
             search_variant.appendChild(variantDiv);
-            search_variant.appendChild(aboutStoreDiv);
+            search_variant.appendChild(book_form_div);
 
-            const inputFields = aboutStoreDiv.querySelectorAll('.book_form_secion_inp');
+            const inputFields = book_form_div.querySelectorAll('.book_form_secion_inp');
             inputFields.forEach((input) => {
                 input.addEventListener('input', () => {
                     const value = input.value;
@@ -134,7 +134,7 @@ async function showBookVariants(books) {
                 });
             });
 
-            const addBtn = aboutStoreDiv.querySelector('.book_form_secion_btn');
+            const addBtn =book_form_div.querySelector('.book_form_secion_btn');
             addBtn.addEventListener('click', () => {
                 const bookData = {
                     title: titleInput,
@@ -144,7 +144,7 @@ async function showBookVariants(books) {
                     bookType: bookTypeInput
                 }
                 addBookToFirebase(bookData);
-                addBtn.textContent='Addet';
+                addBtn.textContent='Added';
                 addBtn.disabled=true;
 
                 titleInput.value='Angels';
@@ -158,6 +158,7 @@ async function showBookVariants(books) {
     });
 }
 
+// Elementin gorsenmeyi ucun funksiya
 function displayFunk(el) {
     let class_List = el.classList;
     if (class_List.contains('d-none')) {
@@ -165,6 +166,7 @@ function displayFunk(el) {
     }
 }
 
+// Firebase'e kitablari atan funksiya
 function addBookToFirebase(bookData) {
     const booksRef = ref(database, 'books');
     push(booksRef, bookData);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
 
 
+      
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getDatabase, ref, orderByChild, startAt, endAt, onValue } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c",
+//     authDomain: "library-35b3c.firebaseapp.com",
+//     projectId: "library-35b3c",
+//     storageBucket: "library-35b3c.appspot.com",
+//     messagingSenderId: "498632706422",
+//     appId: "1:498632706422:web:9d181dd4820520b7c01257",
+//     databaseURL: "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
 
 // const inputSearchEl = document.querySelector('#inputSearchEl');
 // const searchBtnEl = document.querySelector('#searchBtnEl');
 // const bookEl = document.querySelector('#bookEl');
 
+// const databaseRef = ref(database, 'books');
 
-// const databaseRef=ref(database,'books')
 
 
 
@@ -38,3 +56,122 @@
 //     });
 //   }
 // });
+  
+// async function showBookVariants(books) {
+//     search_variant.innerHTML = '';
+//     books.forEach((book) => {
+//         const variantDiv = document.createElement('div');
+//         variantDiv.classList.add('variant');
+//         variantDiv.innerHTML = `
+          
+//             <div class="variant-details">
+//                 <h3>${book.volumeInfo.title}</h3>
+//                 <p>${
+//                     book.volumeInfo.authors
+//                     ? book.volumeInfo.authors.join(', ')
+//                     : 'Unknown Author'
+//                 }</p>
+//                 <p>${book.volumeInfo.publisher || 'Unknown Publisher'}</p>
+//                 <p>${book.volumeInfo.publishedDate || 'Unknown Date'}</p>
+//             </div>
+//         `;
+//         variantDiv.addEventListener('click', () => {
+
+//          search_Input.value=book.volumeInfo.title || "unkown title";
+//             search_variant.innerHTML = ''; // Hazırki məlumatları təmizləyir
+//             variantDiv.classList.remove('variant');
+//             variantDiv.classList.add('selected-variant');
+//             variantDiv.style.width = '100%';
+//             variantDiv.style.height = '100%';
+//             variantDiv.style.padding = '30px';
+//             variantDiv.style.marginLeft = '30px';
+//             variantDiv.style.gap = '40px';
+//             variantDiv.style.marginTop='20px'
+//          fillBookForm(book);
+//          search_variant.parentNode.insertBefore(bookFormContainer, search_variant.nextSibling);
+//          bookFormContainer.appendChild(book_form_div);
+
+//             const titleInput = book.volumeInfo.title || 'Unknown Title';
+//             const imageUrlInput = book.volumeInfo.imageLinks?.thumbnail || '/assets/img/default.png';
+//             const descriptionInput = book.volumeInfo.description || 'No Description Available';
+//             const authorInput = book.volumeInfo.authors || 'Unknown Author';
+//             const bookTypeInput = book.volumeInfo.categories || 'Unknown Type'; 
+
+//             book_form_div.innerHTML = `
+               
+//                     <div class="book_form_secion">
+//                         <h2 class="book_form_secion_title">Book Name</h2>
+//                         <input type="text" class="book_form_secion_inp" value="${titleInput}" placeholder="Angels">
+//                     </div>
+//                     <div class="book_form_secion">
+//                         <h2 class="book_form_secion_title">Author Name</h2>
+//                         <input type="text" class="book_form_secion_inp" value="${authorInput}" placeholder="Dan Brown">
+//                     </div>
+//                     <div class="book_form_secion">
+//                         <h2 class="book_form_secion_title">Book Image Url</h2>
+//                         <input type="text" class="book_form_secion_inp" placeholder="Alex Smith" value="${imageUrlInput}">
+//                     </div>
+//                     <div class="book_form_secion">
+//                         <h2 class="book_form_secion_title"> Description</h2>
+//                         <textarea name="" id="" cols="30" rows="10" class="description_inp" placeholder="We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...We work without holidays and weekends! Resident...">${descriptionInput}</textarea>
+//                     </div>
+//                     <div class="book_form_secion">
+//                         <h2 class="book_form_secion_title">Book Type</h2>
+//                         <input type="text" class="book_form_secion_inp" value="${bookTypeInput}" placeholder="fantastic">
+//                     </div>
+//                     <button class="book_form_secion_btn">add</button>
+               
+//             `;
+
+//             search_variant.appendChild(variantDiv);
+//             search_variant.appendChild(book_form_div);
+
+//             const inputFields =book_form_div.querySelectorAll('.book_form_secion_inp');
+//             inputFields.forEach((input) => {
+//                 input.addEventListener('input', () => {
+//                     const value = input.value;
+//                     const id = input.getAttribute('id');
+//                     const targetInput = book_form_div.querySelector(`#${id}`);
+//                     if (targetInput) {
+//                         targetInput.value = value;
+//                     }
+//                 });
+//             });
+
+//             const addBtn =book_form_div.querySelector('.book_form_secion_btn');
+//             addBtn.addEventListener('click', () => {
+//                 const bookData = {
+//                     title: titleInput,
+//                     author: authorInput,
+//                     imageUrl: imageUrlInput,
+//                     description: descriptionInput,
+//                     bookType: bookTypeInput
+//                 }
+//                 addBookToFirebase(bookData);
+//                 addBtn.textContent='Addet';
+//                 addBtn.disabled=true;
+
+//                 search_Input.value = '';
+              
+//                 titleInput.value='Angels';
+//                 authorInput.value='';
+//                 imageUrlInput.value='';
+//                 descriptionInput.value='';
+//                 bookTypeInput.value='';
+//             });
+//         });
+//         search_variant.appendChild(variantDiv);
+//     });
+// }
+
+// function displayFunk(el) {
+//     let class_List = el.classList;
+//     if (class_List.contains('d-none')) {
+//         el.classList.remove('d-none');
+//     }
+// }
+
+// function addBookToFirebase(bookData) {
+//     const booksRef = ref(database, 'books');
+//     push(booksRef, bookData);
+// }
