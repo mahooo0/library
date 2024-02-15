9d053793e603c873a6896ec0a9117b90f7dd0e99
MuraddAlizada <alizademurad@hotmail.com>
2024-02-15 19:29:04 +0400

    "Replace alerts with notifications for displaying success and error messages"

--- assets/scripts/admin-about.js
+++ assets/scripts/admin-about.js
@@ -13,24 +13,19 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 // Initialize Firebase
-
-
-// console.log("app" , app);
 const firebaseConfig = {
   apiKey: "AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c",
   authDomain: "library-35b3c.firebaseapp.com",
-  databaseURL: "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "library-35b3c",
   storageBucket: "library-35b3c.appspot.com",
   messagingSenderId: "498632706422",
-  appId: "1:498632706422:web:9d181dd4820520b7c01257"
+  appId: "1:498632706422:web:9d181dd4820520b7c01257",
 };
 
-  const app = initializeApp(firebaseConfig);
-
-  const db = getDatabase();
-
-
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
 const aboutInfoBtn = document.querySelector("#about-info-add");
 
@@ -40,38 +35,64 @@ aboutInfoBtn.addEventListener("click", function (event) {
   const aboutBookImg = document.querySelector("#aboutBookImg").value;
   const aboutDesc = document.querySelector("#aboutDesc").value;
 
+  // check if any field is empty
+  if (!aboutTitle || !aboutBookImg || !aboutDesc) {
+    displayNotification("Please fill in all fields!", "error");
+    return; // Stop further execution
+  }
 
-    // check edirik bos xana varsa
-    if (!aboutTitle || !aboutBookImg || !aboutDesc) {
-      alert("Please fill in all fields");
-      return; // Stop further execution
-    }
-
-  //inputa daxil etdiyimiz valuelari objecte ceviririk
+  // Convert input values to an object
   let aboutBookData = {
     aboutTitle,
     aboutBookImg,
     aboutDesc,
   };
-  console.log("newdata" ,aboutBookData );
 
-  //database'da ishlediyimiz qola set edirik
+  // Set the data in the database
   set(ref(db, "about-info/"), aboutBookData)
     .then(() => {
-      alert("Data set successfully!")
+      displaySuccessNotification("Data set successfully!");
       console.log("Data set successfully!");
     })
     .catch((error) => {
-      alert("Error setting data: ")
+      displayNotification("Error setting data: " + error.message, "error");
       console.error("Error setting data: ", error);
     });
 });
 
+function displayNotification(message, type, callback) {
+  const notificationContainer = document.createElement("div");
+  notificationContainer.className = "notification-container";
 
+  const notification = document.createElement("div");
+  notification.className =
+    "notification" + (type === "error" ? " error-notification" : ""); // Added error-notification class if type is 'error'
+  notification.innerHTML = message;
 
+  const timerBar = document.createElement("div");
+  timerBar.className = "timer-bar";
+  notification.appendChild(timerBar);
 
+  notificationContainer.appendChild(notification);
+  document.body.appendChild(notificationContainer);
 
+  let countdown = 5;
+  const timerInterval = setInterval(() => {
+    countdown--;
 
+    if (countdown <= 0) {
+      clearInterval(timerInterval);
+      notification.remove();
+      notificationContainer.remove();
+      if (typeof callback === "function") {
+        callback(); // Invoke callback function upon notification dismissal
+      }
+    } else {
+      timerBar.style.width = (countdown / 5) * 100 + "%";
+    }
+  }, 1000);
+}
 
-
-
+function displaySuccessNotification(message, callback) {
+  displayNotification(message, "success", callback);
+}
--- assets/scripts/join-modal.js
+++ assets/scripts/join-modal.js
@@ -1,85 +1,128 @@
-
-let joinBtn = document.querySelector("#joinBtn");
-let joinModal = document.querySelector("#joinModal");
-document.addEventListener("DOMContentLoaded", function () {
-  //sehife tam acildiqdan sonra ishlesin
-
-  //join buttonuna click etdikde modal acilsin
-  joinBtn.addEventListener("click", function () {
-    if (joinModal.style.display === "none" || joinModal.style.display === "") {
-      joinModal.style.display = "block";
-    } else {
-      joinModal.style.display = "none";
-    }
-  });
-});
-
-//modaldan kenar her hansi bir yere click etdikde modal baglansin
-
-window.addEventListener("click", function (event) {
-  if (event.target === joinModal) {
-    joinModal.style.display = "none";
-  }
-});
-
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
   getDatabase,
   ref,
   push,
-} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c',
-  authDomain: 'library-35b3c.firebaseapp.com',
-  projectId: 'library-35b3c',
-  storageBucket: 'library-35b3c.appspot.com',
-  messagingSenderId: '498632706422',
-  appId: '1:498632706422:web:9d181dd4820520b7c01257',
+  apiKey: "AIzaSyCmrBszyLIOb3kPxG_ou9O99qTBV9s7M3c",
+  authDomain: "library-35b3c.firebaseapp.com",
+  projectId: "library-35b3c",
+  storageBucket: "library-35b3c.appspot.com",
+  messagingSenderId: "498632706422",
+  appId: "1:498632706422:web:9d181dd4820520b7c01257",
   databaseURL:
-    'https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app',
-
+    "https://library-35b3c-default-rtdb.europe-west1.firebasedatabase.app",
 };
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-// export const firebaseDatabase = getDatabase(firebaseApp);
-export const firebaseDatabase = database;
-//join us HTML el
 
-const Join_btn=document.querySelector("#Join_btn")
-//join info fire base
-Join_btn.addEventListener("click",()=>{
-  const Join_name=document.querySelector("#Join_name").value
-  const Join_email=document.querySelector("#Join_email").value
-  if(!Join_name||!Join_email){
-    alert("please fill in all fields! ")
-    return
-  }
-  let join_info={
-    Join_name,
-    Join_email
-  }
-console.log(join_info);
-addJoinInfoToFirebase(join_info)
+function displayNotification(message, type) {
+  const notificationContainer = document.createElement("div");
+  notificationContainer.className = "notification-container";
 
+  const notification = document.createElement("div");
+  notification.className = "notification";
+  notification.innerHTML = message;
 
+  const timerBar = document.createElement("div");
+  timerBar.className = "timer-bar";
+  notification.appendChild(timerBar);
 
+  notificationContainer.appendChild(notification);
+  document.body.appendChild(notificationContainer);
 
+  let countdown = 5;
+  const timerInterval = setInterval(() => {
+    countdown--;
 
-
-joinModal.style.display = "none";
-document.querySelector("#Join_email").value=""
-document.querySelector("#Join_name").value=""
-
-})
-
-async function addJoinInfoToFirebase(Data) {
-  try {
-    const joinRef = ref(database, 'join-info');
-    await push(joinRef, Data);
-    alert("join sucsesfully");
-  } catch (error) {
-    console.error('Error adding book to Firebase:', error);
-    throw error; // 
-  }
+    if (countdown <= 0) {
+      clearInterval(timerInterval);
+      notification.remove();
+      notificationContainer.remove();
+    } else {
+      timerBar.style.width = (countdown / 5) * 100 + "%";
+    }
+  }, 1000);
 }
+
+function displaySuccessNotification(message, type) {
+  const notificationContainer = document.createElement("div");
+  notificationContainer.className = "notification-container";
+
+  const notification = document.createElement("div");
+  notification.className = "notification success-notification"; // Added success-notification class
+  notification.innerHTML = message;
+
+  const timerBar = document.createElement("div");
+  timerBar.className = "timer-bar";
+  notification.appendChild(timerBar);
+
+  notificationContainer.appendChild(notification);
+  document.body.appendChild(notificationContainer);
+
+  let countdown = 5;
+  const timerInterval = setInterval(() => {
+    countdown--;
+
+    if (countdown <= 0) {
+      clearInterval(timerInterval);
+      notification.remove();
+      notificationContainer.remove();
+    } else {
+      timerBar.style.width = (countdown / 5) * 100 + "%";
+    }
+  }, 1000);
+}
+
+const Join_btn = document.querySelector("#Join_btn");
+
+Join_btn.addEventListener("click", async () => {
+  const Join_name = document.querySelector("#Join_name").value;
+  const Join_email = document.querySelector("#Join_email").value;
+
+  if (!Join_name || !Join_email) {
+    displayNotification("Please fill in all fields!", "danger");
+    return;
+  }
+
+  const join_info = {
+    Join_name,
+    Join_email,
+  };
+
+  console.log(join_info);
+
+  try {
+    const joinRef = ref(database, "join-info");
+    await push(joinRef, join_info);
+    displaySuccessNotification("Joined successfully!", "success"); // Call displaySuccessNotification for success
+  } catch (error) {
+    console.error("Error adding join info to Firebase:", error);
+    displayNotification("Error joining!", "danger");
+  }
+
+  document.querySelector("#Join_email").value = "";
+  document.querySelector("#Join_name").value = "";
+});
+
+const joinModal = document.querySelector("#joinModal");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const joinBtn = document.querySelector("#joinBtn");
+
+  joinBtn.addEventListener("click", () => {
+    joinModal.style.display =
+      joinModal.style.display === "none" || joinModal.style.display === ""
+        ? "block"
+        : "none";
+  });
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === joinModal) {
+    joinModal.style.display = "none";
+  }
+});
